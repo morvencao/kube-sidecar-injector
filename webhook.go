82c78779c4afac7430acc21bc111ada8c6d1db42
Marton Sereg <martonsereg@gmail.com>
2018-09-24 13:52:48 +0200

    gofmt

--- webhook.go
+++ webhook.go
@@ -10,12 +10,12 @@ import (
 	"github.com/golang/glog"
 	"k8s.io/api/admission/v1beta1"
 	admissionregistrationv1beta1 "k8s.io/api/admissionregistration/v1beta1"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/kubernetes/pkg/apis/core/v1"
-	appsv1 "k8s.io/api/apps/v1"
 )
 
 var (
