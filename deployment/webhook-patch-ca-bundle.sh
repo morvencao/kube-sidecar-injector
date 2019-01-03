1ba97aaa2fa34211c30327b061c372ad237bc0db
Jason Smith <jasonrichardsmith@gmail.com>
2019-01-03 13:45:13 +0100

    updated to universal command

--- deployment/webhook-patch-ca-bundle.sh
+++ deployment/webhook-patch-ca-bundle.sh
@@ -6,7 +6,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-export CA_BUNDLE=$(kubectl config view --raw -o json | jq -r '.clusters[0].cluster."certificate-authority-data"' | tr -d '"')
+
+export CA_BUNDLE=$(kubectl config view --raw --flatten -o json | jq -r '.clusters[] | select(.name == "'$(kubectl config current-context)'") | .cluster."certificate-authority-data"')
 
 if command -v envsubst >/dev/null 2>&1; then
     envsubst
