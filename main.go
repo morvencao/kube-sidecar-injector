297175e1591c547c45f2d4445e50a2f692050ab2
Ben Wells <b.v.wells@gmail.com>
2019-03-22 16:32:38 +0000

    Fix typos and formatting

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	
+
 	"github.com/golang/glog"
 )
 
@@ -22,42 +22,42 @@ func main() {
 	flag.StringVar(&parameters.keyFile, "tlsKeyFile", "/etc/webhook/certs/key.pem", "File containing the x509 private key to --tlsCertFile.")
 	flag.StringVar(&parameters.sidecarCfgFile, "sidecarCfgFile", "/etc/webhook/config/sidecarconfig.yaml", "File containing the mutation configuration.")
 	flag.Parse()
-	
+
 	sidecarConfig, err := loadConfig(parameters.sidecarCfgFile)
 	if err != nil {
-		glog.Errorf("Filed to load configuration: %v", err)
+		glog.Errorf("Failed to load configuration: %v", err)
 	}
-	
+
 	pair, err := tls.LoadX509KeyPair(parameters.certFile, parameters.keyFile)
 	if err != nil {
-		glog.Errorf("Filed to load key pair: %v", err)
+		glog.Errorf("Failed to load key pair: %v", err)
 	}
-	
-	whsvr := &WebhookServer {
-		sidecarConfig:    sidecarConfig,
-		server:           &http.Server {
-			Addr:        fmt.Sprintf(":%v", parameters.port),
-			TLSConfig:   &tls.Config{Certificates: []tls.Certificate{pair}},
+
+	whsvr := &WebhookServer{
+		sidecarConfig: sidecarConfig,
+		server: &http.Server{
+			Addr:      fmt.Sprintf(":%v", parameters.port),
+			TLSConfig: &tls.Config{Certificates: []tls.Certificate{pair}},
 		},
 	}
-	
+
 	// define http server and server handler
 	mux := http.NewServeMux()
 	mux.HandleFunc("/mutate", whsvr.serve)
 	whsvr.server.Handler = mux
-	
+
 	// start webhook server in new rountine
 	go func() {
 		if err := whsvr.server.ListenAndServeTLS("", ""); err != nil {
-			glog.Errorf("Filed to listen and serve webhook server: %v", err)
+			glog.Errorf("Failed to listen and serve webhook server: %v", err)
 		}
 	}()
-	
+
 	// listening OS shutdown singal
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
 	<-signalChan
-	
-	glog.Infof("Got OS shutdown signal, shutting down wenhook server gracefully...")
+
+	glog.Infof("Got OS shutdown signal, shutting down webhook server gracefully...")
 	whsvr.server.Shutdown(context.Background())
 }
