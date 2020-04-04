86b402083054e286d4b934360e01ce8959787b23
morvencao <morvencao@gmail.com>
2020-04-04 20:42:04 +0800

    fix caBundle issue.

--- deployment/webhook-patch-ca-bundle.sh
+++ deployment/webhook-patch-ca-bundle.sh
@@ -8,6 +8,10 @@ set -o pipefail
 
 export CA_BUNDLE=$(kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[].cluster.certificate-authority-data}')
 
+if [ -z "${CA_BUNDLE}"] then
+    export CA_BUNDLE=$(kubectl get secrets -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='default')].data.ca\.crt}")
+fi
+
 if command -v envsubst >/dev/null 2>&1; then
     envsubst
 else
