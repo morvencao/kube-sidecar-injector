301194a2345708dfb238228a3c746aa3a37a73d5
Tariq Ibrahim <tariq181290@gmail.com>
2020-07-10 16:17:48 -0700

    fix shellcheck issues in CSR scripts

--- deployment/webhook-patch-ca-bundle.sh
+++ deployment/webhook-patch-ca-bundle.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-ROOT=$(cd $(dirname $0)/../../; pwd)
-
 set -o errexit
 set -o nounset
 set -o pipefail
 
-export CA_BUNDLE=$(kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[].cluster.certificate-authority-data}')
+CA_BUNDLE=$(kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[].cluster.certificate-authority-data}')
 
-if [ -z "${CA_BUNDLE}"] then
-    export CA_BUNDLE=$(kubectl get secrets -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='default')].data.ca\.crt}")
+if [ -z "${CA_BUNDLE}" ]; then
+    CA_BUNDLE=$(kubectl get secrets -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='default')].data.ca\.crt}")
 fi
+
+export CA_BUNDLE
 
 if command -v envsubst >/dev/null 2>&1; then
     envsubst
