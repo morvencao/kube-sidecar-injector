4c2537406ebdd6420f009f8d3076b06c2c41553e
morvencao <lcao@redhat.com>
2022-02-24 17:36:20 +0800

    fix signerName in CSR.

--- deploy/webhook-create-signed-cert.sh
+++ deploy/webhook-create-signed-cert.sh
@@ -91,6 +91,7 @@ spec:
   groups:
   - system:authenticated
   request: $(< "${tmpdir}"/server.csr base64 | tr -d '\n')
+  signerName: "kubernetes.io/kube-apiserver-client"
   usages:
   - digital signature
   - key encipherment
