4b685804478156c52535e1a420815fb86fab9fd8
morvencao <lcao@redhat.com>
2022-02-25 16:33:04 +0800

    update repo.
    Signed-off-by: morvencao <lcao@redhat.com>

--- cmd/main.go
+++ cmd/main.go
@@ -37,8 +37,8 @@ func init() {
 func main() {
 	// init command flags
 	flag.IntVar(&port, "port", 8443, "Webhook server port.")
-	flag.StringVar(&webhookServiceName, "service-name", "sidecar-injector-webhook", "Webhook service name.")
-	flag.StringVar(&sidecarConfigFile, "sidecarCfgFile", "/etc/webhook/config/sidecarconfig.yaml", "Sidecar injector configuration file.")
+	flag.StringVar(&webhookServiceName, "service-name", "sidecar-injector", "Webhook service name.")
+	flag.StringVar(&sidecarConfigFile, "sidecar-config-file", "/etc/webhook/config/sidecarconfig.yaml", "Sidecar injector configuration file.")
 	// flag.StringVar(&certFile, "tlsCertFile", "/etc/webhook/certs/cert.pem", "x509 Certificate file.")
 	// flag.StringVar(&keyFile, "tlsKeyFile", "/etc/webhook/certs/key.pem", "x509 private key file.")
 	flag.Parse()
@@ -82,7 +82,7 @@ func main() {
 
 	// define http server and server handler
 	mux := http.NewServeMux()
-	mux.HandleFunc("/mutate", whsvr.serve)
+	mux.HandleFunc(webhookInjectPath, whsvr.serve)
 	whsvr.server.Handler = mux
 
 	// start webhook server in new rountine
